ff2f4e9ebe4df5b6de16cc52116ebd394a4ee09f
Rasel Kabir <md.rasel@technonext.com>
2025-03-15 23:31:12 +0600

    ++ docker compose file added for postgre

--- Services/Inventory.API/Domain/Entities/Product.cs
+++ Services/Inventory.API/Domain/Entities/Product.cs
@@ -1,10 +1,23 @@
-﻿namespace Inventory.API.Domain.Entities
+﻿using Inventory.API.Domain.Entities.SharedEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inventory.API.Domain.Entities
 {
-    public class Product
+    public class Product: BaseEntity
     {
         public long Id { get; set; } 
         public string Name { get; set; } 
         public string SKU { get; set; }  
         public string Description { get; set; }   
+    }
+    public class ProductConfiguration : BaseEntityTypeConfiguration<Product>
+    {
+
+        public override void Configure(EntityTypeBuilder<Product> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable("Products","product");
+        }
     }
 }
