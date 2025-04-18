f312da8f9d89938828c2c6ec8bb5b3a5ebb3d11f
Rasel Kabir <md.rasel@technonext.com>
2025-04-18 12:46:26 +0600

    ++ Relese inventory and clear cart from redis

--- Services/CartService/Helper/Job/RedisKeyExpirationListener.cs
+++ Services/CartService/Helper/Job/RedisKeyExpirationListener.cs
@@ -1,18 +1,22 @@
 ﻿namespace Cart.API.Helper.Job
 {
+    using Cart.API.Domain.Dtos;
+    using Cart.API.Helper.Client;
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
     using StackExchange.Redis;
+    using System.Text.Json;
 
     public class RedisKeyExpirationListener : BackgroundService
     {
         private readonly IConnectionMultiplexer _redis;
         private readonly ILogger<RedisKeyExpirationListener> _logger;
-
-        public RedisKeyExpirationListener(IConnectionMultiplexer redis, ILogger<RedisKeyExpirationListener> logger)
+        private readonly InventoryServiceClient _inventoryClient;
+        public RedisKeyExpirationListener(IConnectionMultiplexer redis, ILogger<RedisKeyExpirationListener> logger, InventoryServiceClient client)
         {
             _redis = redis; 
             _logger = logger;
+            _inventoryClient = client;
         }
         private static string GetCartKey(string sessionId) => $"cart:{sessionId}";
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -31,16 +35,30 @@
                 _logger.LogWarning($"[Redis Expired] Key: {key}");
                 _logger.LogWarning($"[Redis Expired] Channel: {channel}");
 
-                // ➕ Add your custom logic here (e.g., clean up, notifications, etc.)
-
                  var sessionId = key.ToString().Split(":").Last();
                 var cartKey = GetCartKey(sessionId);
+                var cartItems = await _redis.GetDatabase().HashGetAllAsync(cartKey);
+                foreach (var entry in cartItems)
+                {
+                    var item = JsonSerializer.Deserialize<CartItemDto>(entry.Value!);
+                    var inventory = await _inventoryClient.GetInventoryById(Convert.ToInt64(item.InventoryId));
+                    if (item != null)
+                    {
+                        var invObject = new
+                        {
+                            Id = item.InventoryId,
+                            ActionType = 1, // 1 = IN
+                            ProductId = item.ProductId,
+                            Name = inventory.Data.Name,
+                            SKU = inventory.Data.SKU,
+                            Description = inventory.Data.Description,
+                            Quantity = item.Quantity,
+                        };
 
-                // Deletes the entire hash key
+                        var inv = await _inventoryClient.UpdateInventoryAsync(invObject);
+                    }
+                }
                 bool deleted = await _redis.GetDatabase().KeyDeleteAsync(cartKey);
-                // api call to update inventory
-                //if (deleted)
-                // api call to update inventory
             });
         }
     }
